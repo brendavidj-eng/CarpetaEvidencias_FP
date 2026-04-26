aa58bcc476e1671e734f73a8a7951c2039ddb07a
brendavidj-eng <brenda.vidj@unam.edu>
2026-04-26 04:38:14 +0000

    ejercicio

---
+++ Apuntadores/programa1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define TAM 100
+
+void print_dir_array(float array[][TAM], int nr, int nc){
+    float *ptr = &array[0][0];
+    for(int i=0; i<nr*nc; i++){
+        printf("%p \n", (ptr +i));
+    }
+}
+
+void print_array(float array[][TAM], int nr, int nc){
+    float *ptr = &array[0][0];
+    printf ("[ ");
+    for (int i =0; i<nr; i++){
+        printf("%f", *(ptr+i));
+    }
+    printf("]\n");
+}
+
+int main (){
+    float matriz[][TAM] = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f}
+    int matInt[][TAM]={1, 0, 0, 1, 0, 0, 0, 1};
+    float *ptr;
+    //ptr=matriz;
+    ptr = &matriz[0][0];
+
+    //print_dir_array(matriz, 3, 3);
+    print_array(matriz,3,3);
+
+    return 0;
+}
